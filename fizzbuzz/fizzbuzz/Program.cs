3be966fd4ccd97e735fe5e60c46f2cfc9360ff5f
Cschmi22 <119636252+Cschmi22@users.noreply.github.com>
2023-03-16 10:31:16 -0400

    Add files via upload

---
+++ fizzbuzz/fizzbuzz/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            for (int i = 0; i < 100; i++)
+            {
+                num = i;
+                if (num % 3 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                if (num % 5 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                if (num % 3 == 0 && num % 5 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
